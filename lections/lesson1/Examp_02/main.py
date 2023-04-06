b356f334711ac5d8be830520ff86f14cda03e4ca
Yurii <iyureg@gmail.com>
2023-04-06 18:22:00 +0300

    add Examp_02

---
+++ lections/lesson1/Examp_02/main.py
@@ -0,0 +1,32 @@
+
+a = 3.009323
+b = 6.32903404
+print(round(a*b, 3))
+
+iter = 2
+iter += 3 # iter = iter + 3
+iter -= 4 # iter = iter - 4
+iter *= 5 # iter = iter * 5
+iter /= 5 # iter = iter / 5
+iter //= 5 # iter = iter // 5
+iter %= 5 # iter = iter % 5
+iter **= 5 # iter = iter ** 5
+
+a = 1 > 4
+print(a) # False
+# —------------------------------------
+a = 1 < 4 and 5 > 2
+print(a) # True
+# —------------------------------------
+a = 1 == 2
+print(a) # False
+# —------------------------------------
+a = 1 != 2
+print(a) # True
+
+a = 'qwe'
+b = 'qwe'
+print(a == b) # True
+
+a = 1 < 3 < 5 < 10
+print (a) # True
