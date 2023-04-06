635d5dd99ea813a6cdd60def1a350f89f76661e0
Yurii <iyureg@gmail.com>
2023-04-06 22:10:50 +0300

    add Exam_03 if/elif/else/ while/break/else

---
+++ lections/lesson1/Examp_03/main.py
@@ -0,0 +1,24 @@
+
+i = 0
+while i < 5:
+    # if i == 3:
+        # break
+
+    i = i + 1
+else:
+    print('Пожалуй')
+    print('хватит )')
+print(i)
+
+#__________________________________
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это МАША!')
+# elif username == 'Марина':
+#     print('Я так ждал вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар топ :-)')
+
+# else:
+#     print('Привет, ', username)
