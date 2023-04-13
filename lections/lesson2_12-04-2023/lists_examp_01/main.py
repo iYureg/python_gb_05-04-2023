07b981a83867332c46f8f87e26dc4bc3652394f0
Yurii <iyureg@gmail.com>
2023-04-13 15:46:30 +0300

    add lesson2/examp_1

---
+++ lections/lesson2_12-04-2023/lists_examp_01/main.py
@@ -0,0 +1,18 @@
+list_1 = []
+list_1 = list()
+list_1 = [1, 3, 5, 8]
+# print(list_1)
+# print(list_1[0])
+# print(list_1[-1])
+
+list_2 = [1, 5]
+# print(list_2)
+list_2.append(8)
+# print(list_2)
+
+
+list_3 = []
+print(list_3)
+for i in range(5):
+    list_3.append(i)
+    print(list_3)
