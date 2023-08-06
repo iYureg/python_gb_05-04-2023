a0760e6d5bad9fc83f507fec61fd4bfc9b71f001
Yurii <iyureg@gmail.com>
2023-08-06 22:28:38 +0300

    add reading methods

--- pyNotesProj/Notes.py
+++ pyNotesProj/Notes.py
@@ -8,3 +8,29 @@ def show_menu() -> int:
           "6. Закончить работу\n")
     choice = int(input())
     return choice
+
+
+def work_whith_notes():
+    choice = show_menu()
+    phone_book = read_file('notes.csv')
+
+    while (choice != 6):
+        if choice == 1:
+            print_result(phone_book)
+
+        choice = show_menu()
+
+
+def read_file(filename: str) -> list:
+    result = []
+    fields = ['title', "date", "text"]
+    with open(filename, 'r', encoding='utf-8') as data:
+        for line in data:
+            record = dict(zip(fields, line.strip().split(',')))
+            result.append(record)
+    return result
+
+
+def print_result(data):
+    for doc in data:
+        print(*[v for k, v in doc.items()])
