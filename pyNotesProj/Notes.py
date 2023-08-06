dde5419a96b7620c2362ec35fc323ddac699cc37
Yurii <iyureg@gmail.com>
2023-08-06 23:30:03 +0300

    adding notes methods

--- pyNotesProj/Notes.py
+++ pyNotesProj/Notes.py
@@ -1,3 +1,7 @@
+import time
+from datetime import date
+
+
 def show_menu() -> int:
     print("\n>> Menu <<: \n"
           "1. Показать все заметки\n"
@@ -12,21 +16,24 @@ def show_menu() -> int:
 
 def work_whith_notes():
     choice = show_menu()
-    phone_book = read_file('notes.csv')
+    notes = read_file('notes.csv')
 
     while (choice != 6):
         if choice == 1:
-            print_result(phone_book)
+            print_result(notes)
+        elif choice == 3:
+            note = get_new_note()
+            add_note(note, "notes.csv")
 
         choice = show_menu()
 
 
 def read_file(filename: str) -> list:
     result = []
-    fields = ['title', "date", "text"]
+    fields = ['date', "title", "text"]
     with open(filename, 'r', encoding='utf-8') as data:
         for line in data:
-            record = dict(zip(fields, line.strip().split(',')))
+            record = dict(zip(fields, line.strip().split('; ')))
             result.append(record)
     return result
 
@@ -34,3 +41,20 @@ def read_file(filename: str) -> list:
 def print_result(data):
     for doc in data:
         print(*[v for k, v in doc.items()])
+
+
+def get_new_note():
+    res = []
+    res.append(str(date.fromtimestamp(time.time())))
+    res.append(input("Заголовок: "))
+    res.append(input("Текст заметки: "))
+
+    return res
+
+
+def add_note(note, filename):
+    with open(filename, 'a', encoding='utf-8') as data:
+        data.write("; ".join(note) + "\n")
+
+
+work_whith_notes()
